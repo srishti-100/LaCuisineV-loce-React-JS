bb044d256f8f662b21dc5d3c6c72c32a905ab2c8
srishK10 <srishtikulchandani@gmail.com>
2023-12-10 15:08:23 +0530

    backend and frontend integration error

---
+++ frontend/src/axiosConfig.js
@@ -0,0 +1,9 @@
+// import axios from "axios";
+
+// axios.defaults.baseURL =
+//   process.env.NODE_ENV !== "production" ? "http://localhost:5000" : "/";
+
+import axios from "axios";
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV !== "production" ? "http://localhost:5000" : "/";
